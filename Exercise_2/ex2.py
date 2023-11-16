b730e69ba4ae0238a7b5b61f8aafb9d961bfa8c0
HoangNguyendotcom <ndhoang7089@gmail.com>
2023-11-16 12:58:24 +0700

    Setup codespace

--- Exercise_2/ex2.py
+++ Exercise_2/ex2.py
@@ -1,12 +1,12 @@
 # Ex1: Write a NumPy program to reverse an array (first element becomes last).
 # Input: [12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37]
-ex1_arr = [12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37] 
+ex1_arr = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37] 
 def ex1(arr: list) -> list:
     ...
 # Ex2: Write a NumPy program to test whether each element of a 1-D array is also present in a second array
 # Input Array1: [ 0 10 20 40 60]
 #       Array2: [10, 30, 40]
-ex2_arr1 = [ 0 10 20 40 60]
+ex2_arr1 = [ 0, 10, 20, 40, 60]
 ex2_arr2 = [10, 30, 40]
 def ex2(arr1: list, arr2: list) -> list:
     ...
@@ -23,7 +23,7 @@ def ex3(arr: list) -> list:
 # dog: 440
 # people: 312
 # ...
-file = story.txt
+file = "story.txt"
 def ex4(txtfile: str):
     ...
 
