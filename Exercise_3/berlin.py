9bce45ce9f576751f23c1f0266687f4bed991a06
HoangNguyendotcom <ndhoang7089@gmail.com>
2023-11-30 17:34:35 +0700

    Initialize

--- Exercise_3/berlin.py
+++ Exercise_3/berlin.py
@@ -1,11 +1,48 @@
 import cv2
 
-image = cv2.imread("Berlin.jpg")
-print(image.shape)
+file = "Berlin.jpg"
 
 # Task: Do the following image transformation without using any libraries (You could use opencv for exporting images only)
 # 1. Flip the image horizontally, then export to a new image
+def action1(image):
+    ...
+
 # 2. Flip the image vertically, then export to a new image
+def action2(image):
+    ...
+
 # 3. Rotate the image 90 degrees, then export to a new image
+def action3(image):
+    ...
+
 # 4. Rotate the image -90 degrees, then export to a new image
+def action4(image):
+    ...
 # 5. Resize the image from (1600x900) to (800x450)
+def action5(image):
+    ...
+
+def main():
+    image = cv2.imread(file)
+    print(image.shape)
+    while True:
+        act = input("What will you do? ")
+        match act:
+            case "1":
+                action1(image)
+            case "2":
+                action2(image)
+            case "3":
+                action3(image)
+            case "4":
+                action4(image)
+            case "5":
+                action5(image)
+            case "x":
+                break
+            case _:
+                print("Wrong Input")
+            
+
+if __name__ == "__main__":
+    main()
